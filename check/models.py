40a79c4e3158a6c0c04cf49e17cd7eb33c3078cb
Emma Sadler <summonerskies@gmail.com>
2024-12-01 13:52:00 +0000

    Expanded models for chirp_check app

--- check/models.py
+++ check/models.py
@@ -9,10 +9,39 @@ class Checklist(models.Model):
     list_name = models.CharField(max_length=75, blank=False, unique=True, help_text="Name your bird watching trip (Max 75 characters)")
     description = models.CharField(max_length=200, blank=False, unique=False, help_text="Bird Watching Location (Max 200 characters)")
     created_on = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
 
     class Meta:
         ordering = ["-created_on"]
 
+    def get_absolute_url(self):
+        return reverse("list", args=[self.id])
+
     def __str__(self):
         return self.list_name
+
+class Bird(models.Model):
+    """
+    Represents information about bird(s) being added to the checklist.
+    """
+    bird_name = models.CharField(max_length=100, blank=False)
+    status = models.CharField(
+        max_length=8,
+        default="Not Seen",
+        choices=[
+            ("Not Seen", "Not Seen"),
+            ("Spotted", "Spotted"),
+        ],
+        help_text="Did you see any? (Choices: Not Seen, Spotted)"
+    )
+    number_seen = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    check_list = models.ForeignKey(Checklist, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['bird_name', 'check_list'], name='unique_bird_per_checklist')
+        ]
+
+    def __str__(self):
+        return self.bird_name 
--- check/urls.py
+++ check/urls.py
@@ -2,5 +2,16 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    """
+    urls for the web app site
+    """
     path("", views.HomePage.as_view(), name="home"),
+
+    """
+    chirp check checklist
+    """
+
+    """
+    sign up
+    """
 ]
