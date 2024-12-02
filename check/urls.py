d2e67fe9b18960cf78de2171ac16218f73becd25
Emma Sadler <summonerskies@gmail.com>
2024-12-02 11:35:53 +0000

    Added some very simple css and html

--- check/urls.py
+++ check/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.ChecklistView.as_view(), name="index"),
+    path("", views.ChecklistView.as_view(), name="checklist"),
+    path("", views.BirdView.as_view(), name="birds"),
 ]
