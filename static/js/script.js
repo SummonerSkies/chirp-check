afe1f2d09325d6d97e7357f6e5435049a2bca063
Emma Sadler <summonerskies@gmail.com>
2024-12-11 15:41:00 +0000

    updated delete bird on my_checklist to have modal pop-up warning before proceeding

--- static/js/script.js
+++ static/js/script.js
@@ -3,19 +3,3 @@ var message_ele = document.getElementById("msg");
 setTimeout(function() {
   message_ele.style.display = "none";
 }, 3000);
-
-
-// Delete Bird script
-document.addEventListener('DOMContentLoaded', function () {
-    // Get the form element by its ID
-    const deleteBirdForm = document.getElementById('deleteBirdForm');
-    
-    if (deleteBirdForm) {
-        deleteBirdForm.onsubmit = function(event) {
-            const confirmation = confirm("Are you sure you want to remove this bird?");
-            if (!confirmation) {
-                event.preventDefault();
-            }
-        }
-    }
-});
