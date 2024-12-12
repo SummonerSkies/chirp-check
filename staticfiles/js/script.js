cf589bd0456041f218ec40ee07c79f8cba85027d
Emma Sadler <summonerskies@gmail.com>
2024-12-12 17:04:28 +0000

    afternoon save for CSS/HTML updates with bootstrap, and starting html validation

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,5 +1,9 @@
 // Message time limit for visibility
-var message_ele = document.getElementById("msg");
-setTimeout(function() {
-  message_ele.style.display = "none";
-}, 3000);
+document.addEventListener("DOMContentLoaded", function() {
+  var message_ele = document.getElementById("msg");
+  if (message_ele) {
+    setTimeout(function() {
+      message_ele.style.display = "none";
+    }, 3000);
+  }
+});
